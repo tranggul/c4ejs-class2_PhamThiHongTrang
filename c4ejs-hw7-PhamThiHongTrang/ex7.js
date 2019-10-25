a479e6847b3e588bebfba95e88be85a6f36f94d5
tranggul <phamtrangbarca21799@gmail.com>
2019-10-26 03:24:58 +0700

    commit

--- c4ejs-hw7-PhamThiHongTrang/ex7.js
+++ c4ejs-hw7-PhamThiHongTrang/ex7.js
@@ -5,10 +5,12 @@ console.log(e.target);
 })
 button2.addEventListener('click', (e1)=> {
     console.log(e1.target);
+    //<button id="button1">button 1</button>
 })
 var z = document.getElementById('input');
 input.addEventListener('keydown', (e2)=>{
     console.log(e2.key);
+    //value user input
 })
 
     
